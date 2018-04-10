00816b7d0034ee6c2551996ceeb0e42f80b73e28
Adam Walter <Waltxr@users.noreply.github.com>
2018-04-10 14:59:14 -0400

    cleaning up code, ready for production

--- src/components/TopUrlsCell.js
+++ src/components/TopUrlsCell.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { Header, Table } from 'semantic-ui-react'
 
 const TopUrlsCell = props => {
+    let url = `https://shortenurll.herokuapp.com/api/v1/urls${props.shortened_url}`
     return (
       <Table.Row>
         <Table.Cell>
           <Header as="h4">
             <Header.Content>
-              <a href={props.shortened_url} target='_blank'>{props.shortened_url}</a>
+              <a href={url} target='_blank'>{props.shortened_url}</a>
             </Header.Content>
           </Header>
         </Table.Cell>
